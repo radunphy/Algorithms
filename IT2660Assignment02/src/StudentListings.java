6191ac44a5c80341c15cb0ff1180bd1fa59f64c9
Ryan Dunphy <S01131630@acad.tri-c.edu>
2020-02-04 13:46:32 -0500

    Assignment 2 initial upload

---
+++ IT2660Assignment02/src/StudentListings.java
@@ -0,0 +1,13 @@
+// Ryan Dunphy's Assignment 2 ----- IT2660
+// StudentListings Node Class
+
+public class StudentListings {
+    private String name; // Key field
+    private String address;
+    private String number;
+    public StudentListings(String n, String a, String num){
+        name = n;
+        address = a;
+        number = num;
+    }
+}
